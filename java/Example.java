f92c4e78f2a47548b1673bd1a411f27aee9fdd41
wilderbe4st <alantomjose@gmail.com>
2019-08-19 17:20:55 +0530

    sdasd

---
+++ java/Example.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int name = Integer.parseInt(reader.nextLine());
+        System.out.println("Name : "+name);
+    }
+}
